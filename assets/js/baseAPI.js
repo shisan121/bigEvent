8b4797a76c499082fa87cdcbb1e38719504f4032
shisan121 <1872045806@qq.com>
2020-09-03 14:44:00 +0800

    index 分支完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,22 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
 	// 在发起真正的Ajax的请求之前， 同一拼接请求的根路径
+
 	options.url = 'http://ajax.frontend.itheima.net' + options.url
+	// 统一为有权限的接口，设置headers请求头
+	if (options.url.indexOf('/my/') !== -1) {
+		options.headers = {
+			Authorization: localStorage.getItem('token') || ''
+		}
+	}
+	// 设置访问权限，全局统一挂载complete回调函数
+	options.complete = function (res) {
+		// 在complete回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
+		if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+			// 清空token
+			localStorage.removeItem('token')
+			// 跳转到登录界面
+			location.href = '/login.html'
+		}
+	}
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function () {
+	getUserInfo()
+	// 点击退出
+	$('#btnLogoOut').on('click', function () {
+		// 提示用户是否确认退出
+		layer.confirm('是否确认退出?', {
+			icon: 3,
+			title: '提示'
+		}, function (index) {
+			// 确认退出所进行的操作 1.清空本地存储的token；2.重新跳转到登录界面
+			localStorage.removeItem('token')
+			location.href = 'login.html'
+			// 关闭confirm询问框
+			layer.close(index);
+		});
+	})
+})
+
+function getUserInfo() {
+	$.ajax({
+		url: '/my/userinfo',
+		type: 'GET',
+		// headers: {
+		// 	Authorization: localStorage.getItem('token') || ''
+		// },
+		// success: function (res) {
+		// 	console.log(res);
+		// 	if (res.status !== 0) {
+		// 		return layui.layer.msg('获取用户信息')
+		// 	}
+		// 	renderAvatar(res.data)
+		// }
+	})
+}
+
+function renderAvatar(user) {
+	// 1.获取用户昵称或者用户名
+	var name = user.nickname || user.username
+	// 2.设置欢迎的文本
+	$('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+	// 3.按需渲染用户头像
+	if (user.user_pic !== null) {
+		// 3.1渲染图片头像，并显示头像
+		$('.layui-nav-img').attr('src', user.user_pic).show()
+		$('.text-avatar').hide()
+	} else {
+		$('.layui-nav-img').hide()
+		var first = name[0].toUpperCase()
+		$('.text-avatar').html(first).show()
+	}
+
+}
